dc1b84de653cfa819e8eb5f5208a2ac039693da1
kanda1993 <osusyami@gmail.com>
2023-02-03 21:19:59 +0900

    first commit

---
+++ .devcontainer/backend/laravel/init.sh
@@ -0,0 +1,17 @@
+mkdir temp
+cd ./temp
+
+#https://getcomposer.org/download/ にある手順
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+#Global install
+mv composer.phar /usr/local/bin/composer
+
+cd ..
+rm -rf temp
+
+#http://taustation.com/linux-laravel-installation/
+
